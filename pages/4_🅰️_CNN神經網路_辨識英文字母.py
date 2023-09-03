1e881ad688cdf879348fdda69140e9a9cded5ae7
crabtsai <87925842+crabtsai@users.noreply.github.com>
2023-09-03 13:59:15 +0800

    Update 4_🅰️_CNN神經網路_辨識英文字母.py

--- pages/4_🅰️_CNN神經網路_辨識英文字母.py
+++ pages/4_🅰️_CNN神經網路_辨識英文字母.py
@@ -1,5 +1,5 @@
 # pip install streamlit
-pip install streamlit-drawable-canvas
+# pip install streamlit-drawable-canvas
 import streamlit as st 
 from streamlit_drawable_canvas import st_canvas
 from skimage import data, color, io
