ddff97049af8783a74f2f1300da2c40d31937261
crabtsai <87925842+crabtsai@users.noreply.github.com>
2023-10-22 22:50:24 +0800

    Update 7_🐕分類_貓狗辨識_🐱.py

--- pages/7_🐕分類_貓狗辨識_🐱.py
+++ pages/7_🐕分類_貓狗辨識_🐱.py
@@ -3,8 +3,28 @@ from skimage import io, transform
 import numpy as np
 import tensorflow as tf
 
+class CustomLayer(keras.layers.Layer):
+    def __init__(self, sublayer, **kwargs):
+        super().__init__(**kwargs)
+        self.sublayer = sublayer
 
-model = tf.keras.models.load_model('./model/cats_and_dogs_new_2.h5', custom_objects = None)
+    def call(self, x):
+        return self.sublayer(x)
+
+    def get_config(self):
+        base_config = super().get_config()
+        config = {
+            "sublayer": keras.saving.serialize_keras_object(self.sublayer),
+        }
+        return {**base_config, **config}
+
+    @classmethod
+    def from_config(cls, config):
+        sublayer_config = config.pop("sublayer")
+        sublayer = keras.saving.deserialize_keras_object(sublayer_config)
+        return cls(sublayer, **config)
+model = tf.keras.models.load_model('./model/cats_and_dogs_new_2.h5',   custom_objects={"CustomLayer": CustomLayer, "custom_fn": custom_fn}
+)
 
 # Rest of your code...
 
