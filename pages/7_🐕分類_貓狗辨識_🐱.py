ad8eb6deadb80f78d8ef5d53515f87e0d06051a8
crabtsai <87925842+crabtsai@users.noreply.github.com>
2023-10-22 23:10:53 +0800

    Update 7_🐕分類_貓狗辨識_🐱.py

--- pages/7_🐕分類_貓狗辨識_🐱.py
+++ pages/7_🐕分類_貓狗辨識_🐱.py
@@ -2,11 +2,12 @@ import streamlit as st
 from skimage import io, transform
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 import keras
 
+
 # Load the model with the custom layer, custom function, and custom optimizer
-model = tf.contrib.keras.models.load_model('./model/cats_and_dogs_new_2.h5')
+model = load_model('./model/cats_and_dogs_new_2.h5')
 
 st.title("上傳圖片(貓~狗)辨識")
 st.info("因訓練模型(VGG-16)輸入圖片為150*150，輸入圖片狗跟貓比例占比需高")
