601376b25f9edc94c2035d7471cf118a668ad3b1
crabtsai <87925842+crabtsai@users.noreply.github.com>
2023-10-22 23:23:11 +0800

    Update 7_🐕分類_貓狗辨識_🐱.py

--- pages/7_🐕分類_貓狗辨識_🐱.py
+++ pages/7_🐕分類_貓狗辨識_🐱.py
@@ -21,7 +21,8 @@ CLR_METHOD = "triangular"
 
 
 # Load the model with the custom layer, custom function, and custom optimizer
-Vgg16 = keras.models.load_model(model1, compile=False)
+
+Vgg16 = keras.models.load_model('./model/cats_and_dogs_new_2.h5', compile=False)
 Vgg16.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy' 
    , lr_track, keras.metrics.Precision(), keras.metrics.Recall()])
 
