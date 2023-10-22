9b5cb80d426237ec0cf17b9fd93cd4fd2f61e4a3
crabtsai <87925842+crabtsai@users.noreply.github.com>
2023-10-22 22:05:08 +0800

    Update 7_🐕分類_貓狗辨識_🐱.py

--- pages/7_🐕分類_貓狗辨識_🐱.py
+++ pages/7_🐕分類_貓狗辨識_🐱.py
@@ -4,12 +4,8 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.optimizers import RMSprop
 
-# 定義自定義RMSprop
-class CustomRMSprop(RMSprop):
-    pass
-
 # 載入模型時使用custom_objects
-custom_objects = {'CustomRMSprop': CustomRMSprop}
+custom_objects = RMSprop()
 model = tf.keras.models.load_model('./model/cats_and_dogs_new_2.h5', custom_objects=custom_objects)
 
 st.title("上傳圖片(貓~狗)辨識")
