ad83f4c39c824278859de4cb5f860ee1230076c7
crabtsai <87925842+crabtsai@users.noreply.github.com>
2023-10-22 15:55:52 +0800

    Update 8_辨識🛸_yoloV8辨識_🐱.py

--- pages/8_辨識🛸_yoloV8辨識_🐱.py
+++ pages/8_辨識🛸_yoloV8辨識_🐱.py
@@ -27,9 +27,13 @@ if uploaded_file is not None:
     st.image(image, caption="上傳的圖像", use_column_width=True)
 
     # 使用 YOLO 进行对象检测
-    yolo = YOLO("./model/yolov8n.pt")
+    try:
+        model = tf.keras.models.load_model('./model/yolov8n.pt')
+    except Exception as e:
+        print(f"Error loading the model: {e}")
+        raise  # Reraise the exception for more detailed information
+    yolo = YOLO('./model/yolov8n.pt')
     results = yolo.predict(image)  # 对图像进行预测
-
     # 显示检测结果
     st.subheader("檢測結果")
     for result in results.xyxy[0]:
