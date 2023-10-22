7c6efc852dbc3cdaa5056f0868557301f8ac83c1
crabtsai <87925842+crabtsai@users.noreply.github.com>
2023-10-22 23:25:42 +0800

    Update 7_🐕分類_貓狗辨識_🐱.py

--- pages/7_🐕分類_貓狗辨識_🐱.py
+++ pages/7_🐕分類_貓狗辨識_🐱.py
@@ -18,18 +18,14 @@ MIN_LR = 1e-7
 MAX_LR = 1e-3
 CLR_METHOD = "triangular"
 
-
-
 # Load the model with the custom layer, custom function, and custom optimizer
-
 Vgg16 = keras.models.load_model('./model/cats_and_dogs_new_2.h5', compile=False)
-Vgg16.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy' 
-   , lr_track, keras.metrics.Precision(), keras.metrics.Recall()])
+Vgg16.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy', lr_track, keras.metrics.Precision(), keras.metrics.Recall()])
 
 st.title("上傳圖片(貓~狗)辨識")
 st.info("因訓練模型(VGG-16)輸入圖片為150*150，輸入圖片狗跟貓比例占比需高")
 
-uploaded_file = st.file_uploader("上傳圖片(.png)", type=['png','jpg'])
+uploaded_file = st.file_uploader("上傳圖片(.png)", type=['png', 'jpg'])
 if uploaded_file is not None:
     # 讀取上傳的圖像並調整大小
     image = io.imread(uploaded_file)
@@ -44,11 +40,16 @@ if uploaded_file is not None:
     predicted_class = np.argmax(predictions[0])
     print("模型已加載")
     print("模型預測結果：", predictions)
+    
     # 根據模型預測的類別顯示結果
     if predictions <= 0.5:
-        st.header("這是一隻貓！", divider='rainbow')
+        st.markdown("### 這是一隻貓", unsafe_allow_html=True)
     else:
-        st.header("這是一隻狗！", divider='rainbow')
+        st.markdown("### 這是一隻狗", unsafe_allow_html=True)
     
+    # 添加分隔線
+    st.markdown("---")
+
     # 顯示原始上傳圖像
     st.image(image, caption="上傳的圖像", use_column_width=True)
+
