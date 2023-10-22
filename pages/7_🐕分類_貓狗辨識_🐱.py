f029e19ac5f0f45022a9f1570de40c55e08d203f
crabtsai <87925842+crabtsai@users.noreply.github.com>
2023-10-22 22:16:15 +0800

    Update 7_🐕分類_貓狗辨識_🐱.py

--- pages/7_🐕分類_貓狗辨識_🐱.py
+++ pages/7_🐕分類_貓狗辨識_🐱.py
@@ -2,15 +2,8 @@ import streamlit as st
 from skimage import io, transform
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.optimizers import RMSprop
 
-# 定義自定義RMSprop
-class CustomRMSprop(RMSprop):
-    pass
-
-# 載入模型時使用custom_objects
-custom_objects = {'CustomRMSprop': CustomRMSprop}
-model = tf.keras.models.load_model('./model/cats_and_dogs_new_2.h5',custom_objects={'RMSprop': CustomRMSprop})
+model = tf.keras.models.load_model('./model/cats_and_dogs_new_2.h5')
 
 st.title("上傳圖片(貓~狗)辨識")
 st.info("因訓練模型(VGG-16)輸入圖片為150*150，輸入圖片狗跟貓比例占比需高")
